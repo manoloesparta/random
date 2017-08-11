8053ed90c866564ea8fc3ceb8833221c5cfb4f4c
Manolo <manolototopoyo@hotmail.com>
2017-08-11 12:01:40 -0700

    update nmms.py crawler.py mv scouts_class.py mv

--- nmms.py
+++ nmms.py
@@ -1,13 +1,29 @@
 from splinter import Browser
+import time
 
-with Browser('chrome') as browser:
-    # Visit URL
-    for i in range(1000):
-        url = "https://dilean12.sarahah.com/"
-        browser.visit(url)
-        browser.find_by_tag('textarea').fill('Ah que dijo')
-        # Find and click the 'search' button
-        button = browser.find_by_id('Send').click()
+a = 0
+b = 0
+
+num = input('cuantos mensajes: ')
+num = int(num)
+
+while True:
+	try:
+		with Browser('chrome') as browser:
+			for i in range(num):
+				a += 1
+				url = "https://dilean12.sarahah.com/"
+				browser.visit(url)
+				browser.find_by_tag('textarea').fill('Perra')
+				button = browser.find_by_id('Send').click()
+				time.sleep(1)
+				print(a)
+	except AttributeError:
+		b += 1
+		print(b)
+		pass
+	if a > num:
+		break
 
 # brew install chromedriver
 # pip install splinter
