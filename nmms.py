7c7457cecb7b0bbc87c8dba0a53bfc40b4caa84d
Manolo <manolototopoyo@hotmail.com>
2017-08-12 00:55:36 -0700

    nmms

--- nmms.py
+++ nmms.py
@@ -1,29 +1,36 @@
 from splinter import Browser
 import time
 
-a = 0
-b = 0
+def main():
 
-num = input('cuantos mensajes: ')
-num = int(num)
+	a = 0
+	b = 0
 
-while True:
-	try:
-		with Browser('chrome') as browser:
-			for i in range(num):
-				a += 1
-				url = "victima"
-				browser.visit(url)
-				browser.find_by_tag('textarea').fill('mensaje')
-				button = browser.find_by_id('Send').click()
-				time.sleep(1)
-				print(a)
-	except AttributeError:
-		b += 1
-		print(b)
-		pass
-	if a > num:
-		break
+	num = input('cuantos mensajes: ')
+	num = int(num)
+
+	while True:
+		try:
+			with Browser('chrome') as browser:
+				for i in range(num):
+					a += 1
+					url = "victima"
+					browser.visit(url)
+					browser.find_by_tag('textarea').fill('mensaje')
+					button = browser.find_by_id('Send').click()
+					time.sleep(1)
+					print(a)
+					if a > num:
+						break
+		except AttributeError:
+			b += 1
+			print(b)
+			pass
+		if a > num:
+			break
+
+if __name__ == "__main__":
+	main()
 
 # brew install chromedriver
 # pip install splinter
